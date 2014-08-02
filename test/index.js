b84118e3a81230526d17b917a69dcd93e6be1115
Travis Webb <me@traviswebb.com>
2014-08-02 15:33:23 -0400

    added test fixtures, mocha tests in progress

---
+++ test/index.js
@@ -0,0 +1,9 @@
+describe('google-discovery-document', function () {
+  var DiscoveryDocument = require('../');
+
+  describe('#validate', function () {
+    var xtuple_demo_doc = require('./xtuple_demo_pilot');
+
+  });
+
+});
